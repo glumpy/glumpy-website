f8463aff77ea1cc10db5b4632380bcf5f4e9d23b
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2014-09-01 13:08:15 +0200

    Updated website (automated commit)

--- publish.py
+++ publish.py
@@ -99,8 +99,7 @@ if __name__ == '__main__':
     try:
         cd(pages_dir)
         status = sh2('git status | head -1')
-        print status
-        branch = re.match('\# On branch (.*)$', status).group(1)
+        branch = re.match('On branch (.*)$', status).group(1)
         if branch != 'master':
             e = 'On %r, git branch is %r, MUST be "master"' % (pages_dir,
                                                                  branch)
